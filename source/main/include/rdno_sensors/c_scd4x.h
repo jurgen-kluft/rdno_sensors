fd44e8e347b18d508ff88449bfe0c8f084bf9a4d
jurgen.kluft <jurgen.kluft@gmail.com>
2025-05-22 14:19:44 +0800

    * rdno_sensors
    + scd4x sensor

---
+++ source/main/include/rdno_sensors/c_scd4x.h
@@ -0,0 +1,24 @@
+#ifndef __RDNO_SENSORS_SCD4X_H__
+#define __RDNO_SENSORS_SCD4X_H__
+#include "rdno_core/c_target.h"
+#ifdef USE_PRAGMA_ONCE
+#    pragma once
+#endif
+
+namespace ncore
+{
+    class alloc_t;
+
+    namespace nsensors
+    {
+        bool initSCD41(alloc_t* allocator, u8 i2c_address);
+
+        // outHumidity: relative humidity in percent
+        // outTemperature: temperature in degrees Celsius
+        // outCo2: CO2 concentration in ppm
+        bool updateSCD41(f32& outHumidity, f32& outTemperature, u16& outCo2);
+        
+    }  // namespace nsensors
+}  // namespace ncore
+
+#endif  // __RDNO_SENSORS_SCD4X_H__
